b543be8b2be4b88d4012c9832ca0dbf030241d8e
yuki najima <y.najima@kumonosudou.com>
2024-05-04 14:58:22 +0900

    get com.apple.metadata:kMDItemWhereFroms

--- Ex/AppDelegate.swift
+++ Ex/AppDelegate.swift
@@ -91,10 +91,29 @@ class FolderMonitor {
     /// ディレクトリに変更があった際に呼ばれるClosure
     var folderDidChange: (() -> Void)?
 
+    var knowFiles: Set<URL> = []
+
     // MARK: Initializers
 
     init(url: Foundation.URL) {
         self.url = url
+        
+        guard let list = try? FileManager.default.contentsOfDirectory(
+            at: url,
+            includingPropertiesForKeys: [.isDirectoryKey],
+            options: [.skipsSubdirectoryDescendants, .skipsPackageDescendants, .skipsHiddenFiles])
+        else {
+            print("ERR")
+            return
+        }
+        for file in list {
+            print(file)
+            knowFiles.insert(file)
+        }
+        print(list)
+        print(knowFiles)
+
+
     }
 
     // MARK: Monitoring
@@ -109,12 +128,17 @@ class FolderMonitor {
         }
         // Open the directory referenced by URL for monitoring only.
         monitoredFolderFileDescriptor = open(url.path, O_EVTONLY)
+        
         // Define a dispatch source monitoring the directory for additions, deletions, and renamings.
-        folderMonitorSource = DispatchSource.makeFileSystemObjectSource(fileDescriptor: monitoredFolderFileDescriptor,
-                                                                                                     eventMask: .write,
-                                                                                                     queue: folderMonitorQueue)
+        folderMonitorSource = DispatchSource.makeFileSystemObjectSource(
+                fileDescriptor: monitoredFolderFileDescriptor,
+                eventMask: .write,
+                queue: folderMonitorQueue)
+
         // Define the block to call when a file change is detected.
         folderMonitorSource?.setEventHandler { [weak self] in
+            let r = self?.ditectNewFile()
+            print(r)
             self?.folderDidChange?()
         }
 
@@ -136,4 +160,110 @@ class FolderMonitor {
     func stopMonitoring() {
         folderMonitorSource?.cancel()
     }
+
+
+    func getWhereFrom(url: Foundation.URL) -> String? {
+//        guard let values = try? url.resourceValues(forKeys: [.customMetadata]) else {
+//            return nil
+//        }
+//        if let metadata = values.customMetadata {
+//            if let whereFromData = metadata["com.apple.metadata:kMDItemWhereFroms"] as? Data {
+//                if let whereFromString = String(data: whereFromData, encoding: .utf8) {
+//                    return whereFromString
+//                }
+//            }
+//        }
+//
+//
+//        return nil
+        return nil
+    }
+
+
+    func ditectNewFile() -> [URL]? {
+        print("ditectNewFile")
+        guard let currentFiles = getCurrenetFile(url: url) else{
+            return nil
+        }
+
+        let newItems:Set = currentFiles.subtracting(knowFiles)
+
+        for item in newItems {
+            print(item)
+            if let whereFrom = item.whereFrom() {
+                print("whereFrom \(whereFrom)")
+            } else {
+                print("no wherefrom")
+            }
+
+
+
+
+//            guard let attributes = try? FileManager.default.attributesOfItem(atPath: item.path) else {
+//                print("fial")
+//                return nil
+//            }
+//            print("\n attributes")
+//            let valueType = type(of: attributes)
+//            print(valueType)
+//            for attr in attributes {
+//                let key = String(attr.key as NSString)
+//
+//
+//                if key == "NSFileExtendedAttributes" {
+//                    print("\n -- \n")
+//                    print("keystring: \(key) ")
+////                    print(attr.key)
+////                    print(attr.value)
+//                    let valueType = type(of: attr.value)
+//
+//                    if let extendedAttributes = attr.value as? [String: Any] ,
+//                       let whereFromData = extendedAttributes["com.apple.metadata:kMDItemWhereFroms"] as? Data {
+//                        print("extendedAttributes \(type(of:extendedAttributes))")
+//
+//                        print(whereFromData)
+//                        print(type(of: whereFromData))
+//
+//                        let whereFromString = String(decoding: whereFromData, as: UTF8.self)
+//
+//                        print(whereFromData)
+//
+//                    }
+//
+//
+//                    print(valueType)
+//
+//                    print(attr.value)
+//                    print("\n -- \n")
+//                } else {
+//                    print("keystring: \(key) ")
+//                }
+//
+//            }
+//
+//            if let whereFromData = attributes[.extendaa] as? Data {
+//                 if let whereFromString = String(data: whereFromData, encoding: .utf8) {
+//                     print(whereFromString)
+//                 }
+//             }
+
+        }
+        self.knowFiles = newItems
+        
+
+        return Array(newItems)
+    }
+
+    func getCurrenetFile(url: URL) -> Set<URL>? {
+        guard let list = try? FileManager.default.contentsOfDirectory(
+            at: url,
+            includingPropertiesForKeys: [.isDirectoryKey],
+            options: [.skipsSubdirectoryDescendants, .skipsPackageDescendants, .skipsHiddenFiles])
+        else {
+            print("ERR")
+            return nil
+        }
+        return Set(list)
+    }
 }
+
